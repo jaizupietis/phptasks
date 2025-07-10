988539caadc3263ea2e947a276a28c9dbb953d7a
jaizupietis <jaizupietis@gmail.com>
2025-07-10 09:21:42 +0300

    Delete database/add_sample_tasks.sql

--- database/add_sample_tasks.sql
+++
@@ -1,8 +0,0 @@
--- Add sample tasks for mechanic1 (ID should be 3)
-
-USE task_management_db;
-
--- First, let's check user IDs
-SELECT id, username, role FROM users;
-
--- Insert tasks for mechanic1
